76cfa83479027d6d3b893b59885f3330ddd62a69
Charris1904 <a.charris.bueno@gmail.com>
2022-04-13 11:10:03 -0600

    files folder

--- src/POO/polimorfismo/Animal.java
+++ src/POO/polimorfismo/Animal.java
@@ -2,7 +2,19 @@ package POO.polimorfismo;
 
 public class Animal {
 
-    public void sonido(){
+    public void sonido() {
         System.out.println("sonido de un animal");
+    }
+
+    public void mostrar() {
+        System.out.println("No muestra nada");
+    }
+
+    public void mostrar(String texto) {
+        System.out.println("Muestra texto : " + texto);
+    }
+
+    public void mostrar(int numero) {
+        System.out.println("Muestra numero : " + numero);
     }
 }
--- src/POO/polimorfismo/Main.java
+++ src/POO/polimorfismo/Main.java
@@ -3,15 +3,17 @@ package POO.polimorfismo;
 public class Main {
     public static void main(String[] args) {
 
-        Perro perro = new Perro();
-        perro.lamer();
-        perro.sonido();
-
-        //Ejemplo de Polimorfismo
+        //Ejemplo de Polimorfismo - Runtime
         Animal rufus = new Perro();
         rufus.sonido();
 
         rufus = new Gato();
         rufus.sonido();
+
+        //Ejemplo de Polimorfismo - Compile time
+        Animal animal = new Animal();
+        animal.mostrar();
+        animal.mostrar("Hola :)");
+        animal.mostrar(3);
     }
 }
